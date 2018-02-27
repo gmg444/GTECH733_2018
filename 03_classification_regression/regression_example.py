cd106ac2129231b4c0e3452f3479dddbb4f95e48
gmg444 <gmg444gmail.com>
2018-02-27 01:02:00 -0500

    Finished linear regression example.

--- 03_classification_regression/regression_example.py
+++ 03_classification_regression/regression_example.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+from sklearn import linear_model
 
 # Linear regression in plain python
 # From https://machinelearningmastery.com/implement-simple-linear-regression-scratch-python/
-def regression_with_lists(data):
+def linear_regression_with_lists(X, y):
     # Calculate Coefficients
 
     # Calculate the mean value of a list of numbers
@@ -24,51 +24,64 @@ def regression_with_lists(data):
         return sum([(x-mean)**2 for x in values])
 
     # Calculate coefficients
-    def coefficients(dataset):
-        x = [row[0] for row in dataset]
-        y = [row[1] for row in dataset]
+    def coefficients(X, y):
+        x = X.tolist()
+        y = y.tolist()
         x_mean, y_mean = mean(x), mean(y)
         b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
         b0 = y_mean - b1 * x_mean
         return [b0, b1]
 
     # calculate coefficients
-    b0, b1 = coefficients(data)
-    print('Coefficients: B0=%.3f, B1=%.3f' % (b0, b1))
+    b0, b1 = coefficients(X, y)
+    return b0, b1
 
-def regression_with_matrices(data):
+def linear_regression_with_matrices(X, y):
     # Linear regression in numpy
     # From https://thetarzan.wordpress.com/2012/10/27/calculate-ols-regression-manually-in-python-using-numpy/
-
-    ## read data into a Numpy array
-    b1 = np.array(list(data))[1:, 3:5].astype('float')
-
-    nrow = b1.shape[0]
-
+    nrow = y.shape[0]
     intercept = np.ones((nrow, 1))
-    b2 = b1[:, 0].reshape(-1, 1)
 
-    X = np.concatenate((intercept, b2), axis=1)
-    Y = b1[:, 1].T
+    X = np.concatenate((intercept, X), axis=1)
+    Y = y.T
 
-    ## X and Y arrays must have the same number of columns for the matrix multiplication to work:
-    print(X.shape)
-    print(Y.shape)
-
-    ## Use the equation above (X'X)^(-1)X'Y to calculate OLS coefficient estimates:
+    # Use the equation above (X'X)^(-1)X'Y to calculate OLS coefficient estimates:
     bh = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, Y))
-    print (bh)
+    return(bh)
+
+
+def linear_regression_with_gradient_descent(X, y, m_current=0, b_current=0, epochs=10000, learning_rate=0.0001):
+    # Linear regression with gradient descent.
+    # https://towardsdatascience.com/linear-regression-using-gradient-descent-in-10-lines-of-code-642f995339c0
+    N = float(len(y))
+    for i in range(epochs):
+        y_current = (m_current * X) + b_current
+        cost = sum([data ** 2 for data in (y - y_current)]) / N
+        m_gradient = -(2 / N) * sum(X * (y - y_current))
+        b_gradient = -(2 / N) * sum(y - y_current)
+        m_current = m_current - (learning_rate * m_gradient)
+        b_current = b_current - (learning_rate * b_gradient)
+    return m_current, b_current, cost
 
 df_all = pd.read_csv("biomass_predictors_continuous.csv")
 df_all = df_all.sample(frac=1.0, random_state=0)
-
 data = df_all[:10000]
 
 y_train = data["NCBD_30M"].values
-x_train = data.iloc[:, 0:1].values  #Try different columns here
+x_train_2d = data.iloc[:, 0:1].values
+x_train_1d = data["HEIGHT"].values
 
-## check your work with Numpy's built in OLS function:
-regression_with_lists(data)
-regression_with_matrices(data)
-z, resid, rank, sigma = np.linalg.lstsq(x_train, y_train)
-print(z)
+print("\nLinear regression with lists")
+print(linear_regression_with_lists(x_train_1d, y_train))
+
+print("\nLinear regression with matrices")
+print(linear_regression_with_matrices(x_train_2d, y_train))
+
+print("\nLinear regression with scikit learn")
+reg = linear_model.LinearRegression(fit_intercept=True)
+reg.fit(x_train_2d, y_train)
+print(reg.intercept_, reg.coef_)
+
+print("\nLinear regression with gradient descent")
+print(linear_regression_with_gradient_descent(x_train_1d, y_train))
+
